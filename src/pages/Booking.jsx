3947d7b3d1507b5ebfc0aea82e6ee00e9fbcce24
totaltrendhub <arturas.pileckis@gmail.com>
2025-09-03 23:53:28 +0300

    pataisymai2

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -142,7 +142,7 @@ export default function Booking({ workspace }) {
       }
     }
     loadServices();
-  }, [workspace.id]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [workspace.id]);
 
   useEffect(() => {
     async function loadClients() {
